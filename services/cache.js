ef3c2f09ea20be481591881030b9187bde2344f3
Carlos Henrique <chsjr1996@gmail.com>
2020-12-28 00:55:52 -0300

    Try trigger Travis CI

--- services/cache.js
+++ services/cache.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
-const util = require('util');
 const keys = require('../config/keys');
+const { promisify } = require('util');
 
 const client = redis.createClient(keys.redisUrl);
-client.hget = util.promisify(client.hget);
+client.hget = promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.cache = function(options = {}) {
